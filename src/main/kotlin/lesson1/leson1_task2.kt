a902fefbcb972749b105bb859897414f7e835dbf
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-06-06 21:44:26 +0300

    KtSPR-L1-T2-add-workers-count 1) add and change workers count 2) try  to use comments

---
+++ src/main/kotlin/lesson1/leson1_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson1
+
+fun main() {
+    val ordersCount: Int = 75
+    val thanksMsg: String = "Спасибо, что выбираете нас!"
+    println(ordersCount)
+    println(thanksMsg)
+
+    var workersCount = 2000
+    //println(workersCount)
+    workersCount = 1999
+    println(workersCount)
+}
