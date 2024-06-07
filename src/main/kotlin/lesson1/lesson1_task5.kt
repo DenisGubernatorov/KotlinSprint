6c5a7f3ec8be2081794accf999c50507a812995e
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-06-07 17:35:54 +0300

    KtSPR--L1-T5-seconds-to-hh-mm-ss Доработка кода согласно комментарию. Количество секунд в часе и минутах вынесены константы

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,9 +1,13 @@
 package org.example.lesson1
 
+const val SECONDS_PER_HOUR = 3600
+const val SECONDS_PER_MINUTES = 60
+
 fun main() {
+
     val totalSeconds = 6480
-    val hours = totalSeconds / 3600
-    val minutes = (totalSeconds - hours * 3600) / 60
-    val seconds = (totalSeconds - hours * 3600) - minutes * 60
+    val hours = totalSeconds / SECONDS_PER_HOUR
+    val minutes = (totalSeconds - hours * SECONDS_PER_HOUR) / SECONDS_PER_MINUTES
+    val seconds = (totalSeconds - hours * SECONDS_PER_HOUR) - minutes * SECONDS_PER_MINUTES
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
