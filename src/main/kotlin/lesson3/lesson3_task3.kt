1018d3b208de35054d21fa76471eec04aa30e859
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-06-10 23:33:03 +0300

    KtSPR--L3-T3-таблица умножения Вывод таблицы умножения с использованием многострочной интерполяции

---
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -0,0 +1,16 @@
+package lesson3
+
+fun main() {
+    var digit = 2
+    println("""
+        $digit x 1 = ${digit * 1}
+        $digit x 2 = ${digit * 2}
+        $digit x 3 = ${digit * 3}
+        $digit x 4 = ${digit * 4}
+        $digit x 5 = ${digit * 5}
+        $digit x 6 = ${digit * 6}
+        $digit x 7 = ${digit * 7}
+        $digit x 8 = ${digit * 8}
+        $digit x 9 = ${digit * 9}
+    """.trimIndent())
+}
