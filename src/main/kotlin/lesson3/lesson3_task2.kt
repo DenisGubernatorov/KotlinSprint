cc5ff300680196548fb0fa60deee3813d3cc9fa9
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-06-10 23:11:53 +0300

    KtSPR--L3-T2-персолнальные данные Вывод персональных данных с учетом возраста и смены Фамилиии

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package lesson3
+
+fun main() {
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    var lastName = "Андреева"
+    var age = 20
+    println("$firstName $middleName $lastName, $age")
+
+    lastName = "Андреева"
+    age = 22
+    println("$firstName $middleName $lastName, $age")
+
+}
