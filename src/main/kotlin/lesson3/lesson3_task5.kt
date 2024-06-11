e1e4ce5c36dcbb5a3005adb0fd4b7a6632c85244
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-06-11 18:19:31 +0300

    KtSPR--L3-T5--chess-reverse-parsing Парсинг хода. Вариант с использованием split

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -2,14 +2,15 @@ package lesson3
 
 fun main() {
     val receivedData = "D2-D4;16"
+    val splitData = receivedData.split("-", ";")
 
-    val turnFrom = receivedData.substring(0,2)
+    val turnFrom = splitData[0]
     println(turnFrom)
 
-    val turnTo = receivedData.substring(3,5)
+    val turnTo = splitData[1]
     println(turnTo)
 
-    val turnCount = receivedData.substring(6)
+    val turnCount = splitData[2]
     println(turnCount)
 
 }
