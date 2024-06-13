773a4d2491908d76db61ae6dd84e093c2d953952
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-06-13 21:22:14 +0300

    KtSPR--L4-T3--выращивание бобов 1) Добавлены константы допустимых состояний для погоды и тента 2) Текущее состояние погоды и состояние тента переведены на сравнение с допустимыми

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -2,6 +2,9 @@ package lesson4
 
 const val WINTER = "зима"
 const val ACCEPTABLE_HUMIDITY = 20
+const val SHINY_WEATHER = true
+const val AWNING_STATE_OPEN = true
+
 
 fun main() {
     var isShiny = true
@@ -10,7 +13,10 @@ fun main() {
     var season = "зима"
 
     var isFavorableConditions =
-        isShiny && isAwningOpen && humidityPercentage == ACCEPTABLE_HUMIDITY && !WINTER.equals(season, true)
+        isShiny == SHINY_WEATHER && isAwningOpen == AWNING_STATE_OPEN && humidityPercentage == ACCEPTABLE_HUMIDITY && !WINTER.equals(
+            season,
+            true
+        )
     println("Благоприятные ли условия сейчас для роста бобовых? $isFavorableConditions")
 
 }
