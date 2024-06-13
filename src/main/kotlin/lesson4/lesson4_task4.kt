e114fc1c1dd77d97ba1d19ba1d70295102dec5c2
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-06-13 21:31:17 +0300

    KtSPR--L4-T4--тренировки 1) Перевод логики вычисления какое упражнение выполнять с исключающего или на обычное сравнение 2) Вычисление перенесено из отдельных констант сразу в вывод

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -9,18 +9,14 @@ fun main() {
 
     var trainingDay = 5
     // Считаем как обычные люди, начинаем с 1. Таким образом смена упраженений происходит по четными дням тренировки
-    var isEvenTrainingDay = trainingDay.rem(2) == 0
-    var handsEx = isEvenTrainingDay xor handsExFstDay
-    var legsEx = isEvenTrainingDay xor legsExFstDay
-    var backEx = isEvenTrainingDay xor backExFstDay
-    var pressEx = isEvenTrainingDay xor pressExFstDay
+    var isEvenTrainingDay = trainingDay % 2 == 0
 
     println(
         """
-        Упражнения для рук:    $handsEx
-        Упражнения для ног:    $legsEx
-        Упражнения для спины:  $backEx
-        Упражнения для пресса: $pressEx
+        Упражнения для рук:    ${!isEvenTrainingDay == handsExFstDay}
+        Упражнения для ног:    ${!isEvenTrainingDay == legsExFstDay}
+        Упражнения для спины:  ${!isEvenTrainingDay == backExFstDay}
+        Упражнения для пресса: ${!isEvenTrainingDay == pressExFstDay}
     """.trimIndent()
     )
 
