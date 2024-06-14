1470a00f18f53a1d6b2d2d09c1352c488a51c986
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-06-14 16:54:32 +0300

    KtSPR--L5-T1--авторизация Доработки согласно замечаниям 1) Слагаемые перенесены из констант в простые переменные 2) Замена when на if

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,14 +1,15 @@
 package lesson5
 
-const val FIRST_AUTH_NUMBER = 3
-const val SECOND_AUTH_NUMBER = 9
-const val CORRECT_ANSWER = 12
-
 fun main() {
-    print("Для авторизации введите результат выражения $FIRST_AUTH_NUMBER + $SECOND_AUTH_NUMBER = ")
+    val firstAuthNumber = 3
+    val secondAuthNumber = 9
+    val correctAnswer = 12
+
+    print("Для авторизации введите результат выражения $firstAuthNumber + $secondAuthNumber = ")
     val result = readln().toInt()
-    when (result) {
-        CORRECT_ANSWER -> println("Добро пожаловать!")
-        else -> println("Доступ запрещен.")
+    if (result == correctAnswer) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
     }
 }
