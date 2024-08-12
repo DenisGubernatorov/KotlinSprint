56cbfb593b6ab43f4e7ac93e8601aaabcd4fb642
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-12 12:51:08 +0300

    KS-06-02-timer 1)Оптимизация кода. Уход от использования цикла

--- src/main/kotlin/lesson6/lesson6_task2.kt
+++ src/main/kotlin/lesson6/lesson6_task2.kt
@@ -3,9 +3,6 @@ package lesson6
 fun main() {
     print("Введите количество секунд: ")
     val count = readln().toInt()
-
-    for (i in 0..count) {
-        Thread.sleep(1000)
-    }
+    Thread.sleep((1000 * count).toLong())
     println("Прошло $count секунд")
 }
