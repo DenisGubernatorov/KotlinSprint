42a96dc9e8a941fa366fe061e15aebca5e4a7feb
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-12 14:39:55 +0300

    KS-07-01-password-generator 1) Реализация алгоритма генерирующего шестизначный пароль

--- src/main/kotlin/lesson7/lesson7_task1.kt
+++ src/main/kotlin/lesson7/lesson7_task1.kt
@@ -1,2 +1,10 @@
 package lesson7
 
+var password = ""
+
+fun main() {
+    for (i in 0..5) {
+        password += if (i % 2 == 0) ('a'..'z').random() else (1..9).random()
+    }
+    println(password)
+}
