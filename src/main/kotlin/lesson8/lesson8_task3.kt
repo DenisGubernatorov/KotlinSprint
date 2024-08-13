0e97706139632da8aa01fa9034cddb5d5052a845
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-13 23:20:34 +0300

    KS-08-03-ingredients-update 1) Проверка, входит ли ингредиент в рецепт без использования for

--- src/main/kotlin/lesson8/lesson8_task3.kt
+++ src/main/kotlin/lesson8/lesson8_task3.kt
@@ -1,2 +1,20 @@
 package lesson8
 
+fun main() {
+    val marvinFavoriteDish =
+        arrayOf("Черные оливки", "Радиккьо", "Голубой сыр", "Грецкие орехи", "Смородиновый бальзамический уксус")
+    print("Введите название ингредиента: ")
+    val ingredient = readln()
+
+    if (marvinFavoriteDish.any {
+            it.equals(
+                ingredient,
+                ignoreCase = true,
+            )
+        }
+    ) {
+        println("Ингредиент [$ingredient] в рецепте есть")
+    } else {
+        println("Такого ингредиента в рецепте нет")
+    }
+}
