791cfb719662bf768d362ac0e1a967a8c17ed78c
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-14 00:45:29 +0300

    KS-08-05-create-array-of-ingredients Перенос кода в правильный файл

--- src/main/kotlin/lesson8/lesson8_task1.kt
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -1,23 +1,3 @@
 package lesson8
 
-fun main() {
-    val marvinFavoriteDish =
-        arrayOf("Черные оливки", "Радиккьо", "Голубой сыр", "Грецкие орехи", "Смородиновый бальзамический уксус")
-    println("Ингредиенты любимого блюда Марвина")
 
-    marvinFavoriteDish.forEach { println(it) }
-
-    print("Введите ингредиент для замены: ")
-    val toReplace = readln()
-    if (!marvinFavoriteDish.any { it.equals(toReplace, ignoreCase = true) }) {
-        println("Нет такого ингредиента")
-        // завершаем программу
-        return
-    }
-
-    print("Введите название ингредиента на который хотите заменить: ")
-    val replacement = readln()
-    val indexToReplace = marvinFavoriteDish.indexOfFirst { it.equals(toReplace,ignoreCase = true) }
-    marvinFavoriteDish[indexToReplace] = replacement
-    println("Готово! Вы сохранили следующий список: ${marvinFavoriteDish.joinToString(",","[","]")}")
-}
