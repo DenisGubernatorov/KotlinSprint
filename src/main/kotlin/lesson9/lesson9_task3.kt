2ef5215af37cd4d94a8c155db72e86fd41842e3f
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-15 15:17:43 +0300

    KS-09-03-servings-count 1) Расчет необходимого количества ингредиентов на количество порций омлета, указанное пользователем

--- src/main/kotlin/lesson9/lesson9_task3.kt
+++ src/main/kotlin/lesson9/lesson9_task3.kt
@@ -1,2 +1,19 @@
 package lesson9
 
+fun main() {
+    /*
+    Под индексами:
+    0 - яйца
+    1 - молоко
+    2 - сливочное масло
+     */
+    val servings = mutableListOf(2, 50, 15)
+
+    print("Введите количество порций: ")
+    val servingCount = readln().toInt()
+    servings.replaceAll { it * servingCount }
+
+    println(
+        "На $servingCount порций вам понадобится: Яиц – ${servings[0]}, молока – ${servings[1]}, сливочного масла – ${servings[2]}",
+    )
+}
