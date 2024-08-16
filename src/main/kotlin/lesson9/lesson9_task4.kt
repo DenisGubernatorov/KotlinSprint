46a6c775adab3c5b55b264c9f20c8b7494b2816c
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-16 17:02:30 +0300

    KS-09-04-user-ingredients-input 1) Рефакторинг кода. Убрано лишнее преобразование в list

--- src/main/kotlin/lesson9/lesson9_task4.kt
+++ src/main/kotlin/lesson9/lesson9_task4.kt
@@ -3,7 +3,7 @@ package lesson9
 fun main() {
     print("Введите 5 ингредиентов через запятую. После запятой должен быть пробел. Например (ингредиент 1, ингредиент 2, и так далее): ")
     val userIngredients = readln()
-    val splitted = userIngredients.split(", ").toList()
-    val sorted = splitted.sorted()
-    println(sorted)
+    val ingredientsList = userIngredients.split(", ").sorted()
+
+    println(ingredientsList)
 }
