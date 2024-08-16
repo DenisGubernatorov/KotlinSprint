719a4d0b81b6f6041ceb0fc8e7f819c4072fa5df
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-16 18:47:52 +0300

    KS-10-01-humanity-vs-machines 1) Получение случайного числа и вывод инфы о ходе реализовано одним методом. 2) Вычисление победителя также реализовано отдельным методом

--- src/main/kotlin/lesson10/lesson10_task1.kt
+++ src/main/kotlin/lesson10/lesson10_task1.kt
@@ -1,2 +1,27 @@
 package lesson10
 
+import kotlin.random.Random
+
+private const val MACHINE = "машин"
+private const val HUMANITY = "человечества"
+
+fun main() {
+    val machineScore = showAndGetScore(MACHINE)
+    val humanityScore = showAndGetScore(HUMANITY)
+    println(getWinner(machineScore, humanityScore))
+}
+
+private fun getWinner(
+    machineScore: Int,
+    humanityScore: Int,
+) = when {
+    machineScore == humanityScore -> "В этот раз ничья!"
+    machineScore > humanityScore -> "Победили машины"
+    else -> "Победило человечество"
+}
+
+private fun showAndGetScore(player: String): Int {
+    val score = Random.nextInt(1, 7)
+    println("Ход $player. Выпало: $score")
+    return score
+}
