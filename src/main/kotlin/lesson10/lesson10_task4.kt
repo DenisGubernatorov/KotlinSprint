7a45ab2b01775a9bae7a1c1d7ed9417dd0faa3f7
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-17 23:08:00 +0300

    KS-10-04-infinity-war Усовершенствованный вариант игры в кубики 1) После первого раунда, пользователю предлагается продолжить игру. Если отвечает "Да", то игра продолжается 2) При других вариантах ответа игра прекращается и показывается сколько раз выиграл человечество

--- src/main/kotlin/lesson10/lesson10_task4.kt
+++ src/main/kotlin/lesson10/lesson10_task4.kt
@@ -1,2 +1,44 @@
 package lesson10
 
+import kotlin.random.Random
+
+private const val MACHINE = "машин"
+private const val HUMANITY = "человечества"
+private const val MIN_CUBE_VAL = 1
+private const val MAX_CUBE_VAL = 6
+private var humanityWinCount = 0
+
+fun main() {
+    do {
+        doTurn()
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+    } while ("Да".lowercase() == readln().lowercase())
+
+    println("Человечество выиграло $humanityWinCount раз")
+}
+
+private fun doTurn() {
+    val machineScore = getScore()
+    showTurnInfo(MACHINE, machineScore)
+    val humanityScore = getScore()
+    showTurnInfo(HUMANITY, humanityScore)
+
+    when {
+        machineScore > humanityScore -> println("Победили машины")
+        machineScore < humanityScore -> {
+            humanityWinCount++
+            println("Победило человечество")
+        }
+
+        else -> println("В этот раз ничья!")
+    }
+}
+
+private fun showTurnInfo(
+    player: String,
+    score: Int,
+) {
+    println("Ход $player. Выпало: $score")
+}
+
+private fun getScore() = Random.nextInt(MIN_CUBE_VAL, MAX_CUBE_VAL + 1)
