c2840566d222bb5240609d9909d3e0ad83ff9bcc
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-17 10:59:51 +0300

    KS-10-02-user-validate 1) Проверка длины логина и пароля пользователя. Если хотя бы одно значение меньше заданной минимальной длины, то выводится соответствующее сообщение

--- src/main/kotlin/lesson10/lesson10_task2.kt
+++ src/main/kotlin/lesson10/lesson10_task2.kt
@@ -1,2 +1,19 @@
 package lesson10
 
+private const val MIN_LENGTH = 4
+
+fun main() {
+    print("Введите логин: ")
+    val usrName = readln()
+    print("Введите пароль: ")
+    val usrPassword = readln()
+    if (isCorrect(usrName) &&
+        isCorrect(usrPassword)
+    ) {
+        println("Добро пожаловать $usrName")
+    } else {
+        println("Логин или пароль недостаточно длинные")
+    }
+}
+
+private fun isCorrect(str: String) = str.length >= MIN_LENGTH
