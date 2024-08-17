d7e15f9fca1d6380589dd67d85ea0553e707ddcc
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-18 00:39:38 +0300

    KS-10-05-online-shop-authorization Реализация алгоритма авторизации и получения списка товаров корзины 1) Запрашивает логин и пароль 2) Генерирует токен если совпадают логин и пароль. Если не совпадают, то возвращает null 3) Если токен не null. то выводит содержимое корзины. В противном случае сообщает о том, что не удалось авторизоваться

--- src/main/kotlin/lesson10/lesson10_task5.kt
+++ src/main/kotlin/lesson10/lesson10_task5.kt
@@ -1,2 +1,38 @@
 package lesson10
 
+private const val LOGIN = "login"
+private const val PASSWORD = "password"
+private val basket = listOf("Полотенце", "Путеводитель")
+private const val TOKEN_LENGTH = 32
+
+fun main() {
+    print("Введите логин: ")
+    val usrLogin = readln()
+    print("Введите пароль: ")
+    val usrPassword = readln()
+    val generatedToken = getGeneratedToken(usrLogin, usrPassword)
+
+    val basketItems = getBasketItems(generatedToken)
+
+    println(
+        if (basketItems == null) "Не удалось авторизоваться. Проверьте правильность логина или пароля" else "$basketItems",
+    )
+}
+
+fun getBasketItems(generatedToken: String?): List<String>? = if (generatedToken == null) null else basket
+
+fun getGeneratedToken(
+    usrLogin: String,
+    usrPassword: String,
+): String? {
+    if (usrLogin != LOGIN || usrPassword != PASSWORD) return null
+
+    val possibleVals = ('A'..'Z') + ('a'..'z') + ('1'..'9')
+    val tokenAsArray = CharArray(TOKEN_LENGTH)
+    repeat(TOKEN_LENGTH) {
+        tokenAsArray[it] = possibleVals.random()
+    }
+    tokenAsArray.shuffle()
+
+    return String(tokenAsArray)
+}
