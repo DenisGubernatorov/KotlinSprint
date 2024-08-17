e187a1c164e13a7cd41ac31fd5755addeca9f9c0
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-17 12:22:54 +0300

    KS-10-03-password-generator 1) Реализация функции генерации пароля с заданным пользователям количеством символов. Функция не гарантирует, что в начале или конце не будет пробела

--- src/main/kotlin/lesson10/lesson10_task3.kt
+++ src/main/kotlin/lesson10/lesson10_task3.kt
@@ -1,2 +1,22 @@
 package lesson10
 
+fun main() {
+    print("Введите длину пароля: ")
+    println("Ваш пароль: ${generatePassword(readln().toInt())}")
+}
+
+/**
+ * Функция не гарантирует, что в начале или конце не будет пробела. Из-за чего визуально может казаться, что в пароле
+ * меньше знаков, чем указали
+ */
+private fun generatePassword(passwordLength: Int): String {
+    val range = "123456789!\"#\$%&'()*+,-./ "
+    val tmpPassword = CharArray(passwordLength)
+
+    repeat(passwordLength) {
+        tmpPassword[it] = range.random()
+    }
+    tmpPassword.shuffle()
+
+    return String(tmpPassword)
+}
