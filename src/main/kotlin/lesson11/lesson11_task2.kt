5efd50913cdad8051acf33582ba369b4b5a2c5e7
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-18 21:21:28 +0300

    KS-11-02-class-user-with-optional-property 1) Реализация класса user2 с опциональным свойством "о себе"(bio) 2) Реализация метода показывающего инфу об объекте и сеттеров для свойств bio и password

--- src/main/kotlin/lesson11/lesson11_task2.kt
+++ src/main/kotlin/lesson11/lesson11_task2.kt
@@ -1,2 +1,44 @@
 package lesson11
 
+private class User2(
+    var id: Int,
+    var login: String,
+    private var password: String, // ограничиваем получение пароля
+    var email: String,
+    var bio: String? = null,
+) {
+    fun showUserInfo() {
+        println(
+            """Пользователь $login: 
+             id: $id             
+             password: $password
+             email: $email
+             о себе: $bio
+    """,
+        )
+    }
+
+    fun setBio() {
+        print("О себе(введите текст): ")
+        bio = readln()
+    }
+
+    fun setPassword() {
+        print("Для изменения пароля введите старый пароль: ")
+        if (password == readln()) {
+            print("Введите новый пароль: ")
+            password = readln()
+            println("Пароль успешно изменен")
+        } else {
+            println("Неверный пароль")
+        }
+    }
+}
+
+fun main() {
+    val user = User2(1, "login", "password", "usermail@mail.ru")
+
+    user.setBio()
+    user.setPassword()
+    user.showUserInfo()
+}
