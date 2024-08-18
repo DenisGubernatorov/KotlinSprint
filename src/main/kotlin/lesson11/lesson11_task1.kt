2d85de45f578e3e654b8cc426b4d8737037bbb13
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-18 20:16:28 +0300

    KS-11-01-two-entity-of-class-user 1) Определен класс User, со своими свойствами 2) Создание 2х объектов класса User  и вывод значений свойств объектов в консоль

--- src/main/kotlin/lesson11/lesson11_task1.kt
+++ src/main/kotlin/lesson11/lesson11_task1.kt
@@ -1,2 +1,27 @@
 package lesson11
 
+class User(
+    var id: Int,
+    var login: String,
+    var password: String,
+    var email: String,
+)
+
+fun main() {
+    val userOne = User(1, "user1", "pass1", "usr1@mail.ru")
+    val userTwo = User(2, "user2", "pass2", "usr2@mail.ru")
+
+    showInfo(userOne)
+    showInfo(userTwo)
+}
+
+private fun showInfo(user: User) {
+    println(
+        """Пользователь: 
+             id: ${user.id}
+             login: ${user.login}
+             password: ${user.password}
+             email: ${user.email}            
+    """,
+    )
+}
