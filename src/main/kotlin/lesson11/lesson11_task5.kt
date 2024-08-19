2453ebdac4dbeba7670b679d51ec59b8f5bbc5ae
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-20 01:02:54 +0300

    KS-11-05-forum Реализация форума 1) Создание и добавление user сделано через паттерн Фабрика. 2) Сообщения добавляются только пользователь есть в списке участников форума 3) Вывод всех сообщений участника форума

--- src/main/kotlin/lesson11/lesson11_task5.kt
+++ src/main/kotlin/lesson11/lesson11_task5.kt
@@ -1,2 +1,51 @@
 package lesson11
 
+class Forum(
+    private val users: MutableList<User> = mutableListOf(),
+    private val messages: MutableList<Message> = mutableListOf(),
+) {
+    fun createUser(userName: String): User {
+        val id = (users.lastOrNull()?.userId ?: 0) + 1
+        val user = User(id, userName)
+        users.add(user)
+        return user
+    }
+
+    fun createMessage(
+        userId: Int,
+        message: String,
+    ) {
+        users.find { it.userId == userId }?.let { messages.add(Message(userId, message)) }
+            ?: run { println("Нет такого пользователя. Сообщение не добавлено") }
+    }
+
+    fun printThread() {
+        // Красиво, но мне пока читать это еще трудней, чем писать)))
+        messages.forEach { message -> println("${(users.find { message.authorId == it.userId })?.userName}:${message.message}") }
+    }
+
+    data class User(
+        val userId: Int,
+        val userName: String,
+    )
+
+    data class Message(
+        val authorId: Int,
+        val message: String,
+    )
+}
+
+fun main() {
+    val forum = Forum()
+    val monkey1 = forum.createUser("Monkey1")
+    val monkey2 = forum.createUser("Monkey2")
+    // Создать то, из вне все равно можно. Но его сообщения не попадут в список сообщений.
+    val user = Forum.User(3, "Marvin")
+
+    forum.createMessage(monkey1.userId, "Hey!")
+    forum.createMessage(monkey2.userId, "Ho!")
+    forum.createMessage(monkey1.userId, "Beer?")
+    forum.createMessage(monkey2.userId, "Two beer!!!")
+
+    forum.printThread()
+}
