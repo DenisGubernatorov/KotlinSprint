c83b00f4897e82fb4564afb96d891e24e3dae190
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-19 21:20:26 +0300

    KS-11-01-two-entity-of-class-user 1) showInfo переименован в showUserInfo и перенесен из main в класс User

--- src/main/kotlin/lesson11/lesson11_task1.kt
+++ src/main/kotlin/lesson11/lesson11_task1.kt
@@ -1,27 +1,26 @@
 package lesson11
 
 class User(
-    var id: Int,
-    var login: String,
-    var password: String,
-    var email: String,
-)
+    private var id: Int,
+    private var login: String,
+    private var password: String,
+    private var email: String,
+) {
+    fun showUserInfo() {
+        println(
+            """Пользователь $login: 
+             id: $id             
+             password: $password
+             email: $email            
+    """,
+        )
+    }
+}
 
 fun main() {
     val userOne = User(1, "user1", "pass1", "usr1@mail.ru")
     val userTwo = User(2, "user2", "pass2", "usr2@mail.ru")
 
-    showInfo(userOne)
-    showInfo(userTwo)
-}
-
-private fun showInfo(user: User) {
-    println(
-        """Пользователь: 
-             id: ${user.id}
-             login: ${user.login}
-             password: ${user.password}
-             email: ${user.email}            
-    """,
-    )
+    userOne.showUserInfo()
+    userTwo.showUserInfo()
 }
