2cbccc109f025ae9278e341f7ead6fee071b9e33
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-20 22:18:08 +0300

    KS-12-01-weather 1) Создан класс Weather, со значениями по умолчанию и методом для вывода инфы о погоде 2) Созданы два объекта класса Weather, в которых устанавливаются свои значения

--- src/main/kotlin/lesson12/lesson12_task1.kt
+++ src/main/kotlin/lesson12/lesson12_task1.kt
@@ -1,2 +1,33 @@
 package lesson12
 
+class Weather {
+    var dayTemp = 20
+    var nightTemp = 10
+    var isRainFall = false
+
+    fun getWeather() {
+        println(
+            """
+            День: $dayTemp
+            Ночь: $nightTemp
+            Осадки: ${
+                if (isRainFall) "Да" else "Нет"
+            }
+            """.trimIndent(),
+        )
+    }
+}
+
+fun main() {
+    val dayOne = Weather()
+    dayOne.dayTemp = 30
+    dayOne.nightTemp = 15
+    dayOne.isRainFall = true
+    dayOne.getWeather()
+
+    val dayTwo = Weather()
+    dayTwo.dayTemp = 25
+    dayTwo.nightTemp = 5
+    dayTwo.isRainFall = false
+    dayTwo.getWeather()
+}
