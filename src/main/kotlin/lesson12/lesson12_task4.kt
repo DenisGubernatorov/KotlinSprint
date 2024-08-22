d03efb528ab2b9d00453700f05b114851416c753
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-22 21:28:06 +0300

    KS-12-04-Weather-init-block 1) Рефакторинг класса погоды, что бы информация о погоде выводилась сразу при создании объекта

--- src/main/kotlin/lesson12/lesson12_task4.kt
+++ src/main/kotlin/lesson12/lesson12_task4.kt
@@ -1,2 +1,29 @@
 package lesson12
 
+import kotlin.math.roundToInt
+
+class WeatherFour(
+    _dayTemp: Int,
+    _nightTemp: Int,
+    _isRainFall: Boolean,
+) {
+    private var dayTemp: Int = (_dayTemp - 273.15).roundToInt()
+    private var nightTemp: Int = (_nightTemp - 273.15).roundToInt()
+    private var isRainFall: Boolean = _isRainFall
+
+    init {
+        println(
+            """
+            День: $dayTemp
+            Ночь: $nightTemp
+            Осадки: ${
+                if (isRainFall) "Да" else "Нет"
+            }
+            """.trimIndent(),
+        )
+    }
+}
+
+fun main() {
+    WeatherFour(300, 290, false)
+}
