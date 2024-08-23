669daf66038bda136cc4f7fba0ad30d6451b1ec1
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-23 21:47:06 +0300

    KS-13-03-phone-directory-three 1) Компании добавляются в общий список 2) Выводятся названия только существующих компаний

--- src/main/kotlin/lesson13/lesson13_task1.kt
+++ src/main/kotlin/lesson13/lesson13_task1.kt
@@ -1,2 +1,43 @@
 package lesson13
 
+class PhoneDirectory(
+    val name: String,
+    val phone: Long,
+    val companyName: String?,
+) {
+    fun showNumberInfo() {
+        println("Имя: $name \nНомер: $phone\nКомпания: ${companyName ?: "<не указано>"}")
+    }
+}
+
+fun main() {
+    val phonesList =
+        mutableListOf(
+            PhoneDirectory(
+                "John1",
+                89123456789,
+                null,
+            ),
+            PhoneDirectory(
+                "John2",
+                89123456799,
+                null,
+            ),
+            PhoneDirectory(
+                "John3",
+                89123456999,
+                "null",
+            ),
+            PhoneDirectory(
+                "John4",
+                89123459999,
+                "companyName1",
+            ),
+            PhoneDirectory(
+                "John5",
+                89123499999,
+                "companyName2",
+            ),
+        )
+    println(phonesList.filter { it.companyName != null }.map { it.companyName })
+}
