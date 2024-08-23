45024cdbc0b7e4a3fc25b0c7ca5067ffe31e8a03
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-23 23:06:22 +0300

    KS-13-04-phone-directory-four 1) Данные для создания записи в справочнике вводятся через консоль 2) Если пользователь не ввел телефон или ввел не цифры, то запись не создается. Выводится сообщение о невозможности создания. Далее спрашивает продолжить или нет 3) Если пользователь не ввел название компании, то вместо нее записывается null 4) После создания записи в справочнике пользователя спрашивает продолжить или нет

--- src/main/kotlin/lesson13/lesson13_task4.kt
+++ src/main/kotlin/lesson13/lesson13_task4.kt
@@ -1,2 +1,43 @@
 package lesson13
 
+class PhoneDirectoryFour(
+private val name: String,
+private val phone: Long,
+private val companyName: String?,
+) {
+    fun showNumberInfo() {
+        println("Имя: $name \nНомер: $phone\nКомпания: ${companyName ?: "<не указано>"}")
+    }
+}
+
+fun main() {
+    val phoneList = mutableListOf<PhoneDirectoryFour>()
+
+    while (true) {
+        val phone = getInputVal("номер телефона").toLongOrNull()
+
+        if (phone != null) {
+            val name = getInputVal("имя")
+            val companyName = getInputVal("имя компании").takeIf { it.isNotBlank() }
+            // Странно что тут идешка ругается на "Smart cast to kotlin.Long (for null call)" для phone. Хотя здесь
+            // гарантированно не null. Почему так?
+            phoneList.add(PhoneDirectoryFour(name, phone, companyName))
+        } else {
+            println("Не задан или не правильный формат номера телефона. Невозможно добавить в справочник")
+        }
+
+        print("Хотите продолжить ввод (Д/Н)")
+        if ("Д".lowercase() != readln().lowercase()) break
+    }
+
+    println()
+    phoneList.map {
+        it.showNumberInfo()
+        println("----//----")
+    }
+}
+
+fun getInputVal(description: String): String {
+    print("Введите $description: ")
+    return readln()
+}
