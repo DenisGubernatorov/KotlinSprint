1930ed62e0aff96294998973d9083eaf802e128f
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-26 21:45:29 +0300

    KS-15-02-Weather-Station-Stats 1) Реализация абстрактного класса WeatherStationStats имплементирующего интерфейс DataSend с методом отправки данных 2) Класс Temperature, переопределяющий логику отправки данных 3) 2) Класс PrecipitationAmount, переопределяющий логику отправки данных

--- src/main/kotlin/lesson15/lesson15_task2.kt
+++ src/main/kotlin/lesson15/lesson15_task2.kt
@@ -1,1 +1,26 @@
 package lesson15
+
+abstract class WeatherStationStats : DataSend
+
+interface DataSend {
+    fun doSendData()
+}
+
+class Temperature : WeatherStationStats() {
+    override fun doSendData() {
+        println("Отправляем температуру")
+    }
+}
+
+class PrecipitationAmount : WeatherStationStats() {
+    override fun doSendData() {
+        println("Отправляем количество осадков ")
+    }
+}
+
+fun main() {
+    val temperature = Temperature()
+    temperature.doSendData()
+    val precipitationAmount = PrecipitationAmount()
+    precipitationAmount.doSendData()
+}
