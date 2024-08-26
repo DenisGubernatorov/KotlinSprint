4913a67211aedce5c11e04ab5104ccc1424c82a1
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-26 21:19:07 +0300

    KS-15-01-animals-move 1) Реализованы интерфейсы Fly и Swim  с дефолтной реализацией методов doFle и doSwim  соответственно 2) Создан классы Carp(Карась) Seagull(Чайка) и Duck(Утка)  которые реализуют интерфейсы типа передвижения

--- src/main/kotlin/lesson15/lesson15_task1.kt
+++ src/main/kotlin/lesson15/lesson15_task1.kt
@@ -1,1 +1,42 @@
 package lesson15
+
+interface Fly {
+    val name: String
+
+    fun doFly() {
+        println("${this.name} летит")
+    }
+}
+
+interface Swim {
+    val name: String
+
+    fun doSwim() {
+        println("${this.name} плывет")
+    }
+}
+
+class Carp(
+    override val name: String,
+) : Swim
+
+class Seagull(
+    override val name: String,
+) : Fly
+
+class Duck(
+    override val name: String,
+) : Swim,
+    Fly
+
+fun main() {
+    val carp = Carp("Карп")
+    carp.doSwim()
+
+    val seagull = Seagull("Чайка")
+    seagull.doFly()
+
+    val duck = Duck("Утка")
+    duck.doFly()
+    duck.doSwim()
+}
