8d1ffbc0f187fefb086d6cbbd15337f80d8e4b04
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-27 22:34:30 +0300

    KS-15-04-Music-Shop 1) Интерфейс для реализации метода поиска аксессуара для инструмента 2) Класс Instrument. Поля класса название инструмента и количество на складе. Реализует интерфейс поиска аксессуара. 3) Класс Accessories. Поля класса название инструмента и количество на складе.

--- src/main/kotlin/lesson15/lesson15_task4.kt
+++ src/main/kotlin/lesson15/lesson15_task4.kt
@@ -1,1 +1,25 @@
 package lesson15
+
+interface Search {
+    fun doSearch(
+        instrument: Instrument,
+        accessories: Accessories,
+    )
+}
+
+class Accessories(
+    val name: String,
+    val storageCount: Int,
+)
+
+class Instrument(
+    val name: String,
+    val storageCount: Int,
+) : Search {
+    override fun doSearch(
+        instrument: Instrument,
+        accessories: Accessories,
+    ) {
+        println("Выполняется поиск")
+    }
+}
