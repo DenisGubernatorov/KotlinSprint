c5d4afe3d3d02e8c468fc2ee8164107b5001cf3b
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-29 11:39:06 +0300

    KS-15-02-Weather-Station-Stats 1) Рефакторинг согласно ТЗ 2) Добавлен класс WeatherServer, отвечающий за отправку сообщений.

--- src/main/kotlin/lesson15/lesson15_task2.kt
+++ src/main/kotlin/lesson15/lesson15_task2.kt
@@ -1,24 +1,45 @@
-package lesson15
+import java.time.LocalDateTime
 
-abstract class WeatherStationStats {
-    abstract fun doSendData()
-}
+abstract class WeatherStationStats(
+    open val stationId: String,
+    open val dateTime: LocalDateTime,
+)
 
-class Temperature : WeatherStationStats() {
-    override fun doSendData() {
-        println("Отправляем температуру")
+data class Temperature(
+    override val stationId: String,
+    override val dateTime: LocalDateTime,
+    val value: Double,
+) : WeatherStationStats(stationId, dateTime)
+
+data class PrecipitationAmount(
+    override val stationId: String,
+    override val dateTime: LocalDateTime,
+    val value: Double,
+) : WeatherStationStats(stationId, dateTime)
+
+class WeatherServer {
+    fun sendMessage(message: WeatherStationStats) {
+        when (message) {
+            is Temperature -> sendTemperatureData(message)
+            is PrecipitationAmount -> sendPrecipitationData(message)
+        }
     }
-}
 
-class PrecipitationAmount : WeatherStationStats() {
-    override fun doSendData() {
-        println("Отправляем количество осадков ")
+    private fun sendTemperatureData(temperature: Temperature) {
+        println("Отправка значения температуры на сервер: станция ${temperature.stationId}, ")
+    }
+
+    private fun sendPrecipitationData(precipitation: PrecipitationAmount) {
+        println("Отправка количества осадков на сервер: $precipitation")
     }
 }
 
 fun main() {
-    val temperature = Temperature()
-    temperature.doSendData()
-    val precipitationAmount = PrecipitationAmount()
-    precipitationAmount.doSendData()
+    val dateNow = LocalDateTime.now()
+    val temperatureData = Temperature("Station1", dateNow, 25.5)
+    val precipitationData = PrecipitationAmount("Station1", dateNow, 10.2)
+
+    val server1 = WeatherServer()
+    server1.sendMessage(temperatureData)
+    server1.sendMessage(precipitationData)
 }
