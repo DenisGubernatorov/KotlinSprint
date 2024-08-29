04385dffd1bc0ba760d34b9e5b55e2e1a5e1fb88
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-29 22:31:10 +0300

    KS-16-01-playing-dice 1) Класс PlayingDice с приватным полем, где генерируется результат броска кубика от 1 до 6. Реализован публичный метод для вывода результата броска

--- src/main/kotlin/lesson16/lesson16_task1.kt
+++ src/main/kotlin/lesson16/lesson16_task1.kt
@@ -1,1 +1,16 @@
 package lesson16
+
+import kotlin.random.Random
+
+class PlayingDice {
+    private var diceRollResult = Random.nextInt(1, 7)
+
+    fun showDiceRollResult() {
+        println("Выпало: $diceRollResult")
+    }
+}
+
+fun main() {
+    val dice = PlayingDice()
+    dice.showDiceRollResult()
+}
