89528b7a804a4dba83c6737004777710b3da47c8
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-29 22:55:54 +0300

    KS-16-02-Circle 1) Класс Circle принимающий на вход радиус 2) PI сделано приватной константой для файла урока 3) Реализованы методы возвращающие длину окружности и площадь окружности

--- src/main/kotlin/lesson16/lesson16_task2.kt
+++ src/main/kotlin/lesson16/lesson16_task2.kt
@@ -1,1 +1,19 @@
 package lesson16
+
+import kotlin.math.pow
+
+private const val PI = 3.14
+
+class Circle(
+    private val radius: Int,
+) {
+    fun getCircleLength(): Double = 2 * PI * radius
+
+    fun getCircleSquare(): Double = PI * radius.toDouble().pow(2.0)
+}
+
+fun main() {
+    val circle = Circle(4)
+    println(circle.getCircleLength())
+    println(circle.getCircleSquare())
+}
