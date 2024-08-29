37801a7dd89d12a31171e08fc923bac5d8a96651
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-30 00:00:05 +0300

    KS-15-02-Weather-Station-Stats 1) При отправке данных добавлено сообщение для неизвестного типа данных 2) Для тестов добавлен класс Freezing 3) Поправлено сообщения для отправки количества осадков. Вместо объекта PrecipitationAmount выводится id станции с которой они получены

--- src/main/kotlin/lesson15/lesson15_task2.kt
+++ src/main/kotlin/lesson15/lesson15_task2.kt
@@ -17,11 +17,18 @@ data class PrecipitationAmount(
     val value: Double,
 ) : WeatherStationStats(stationId, dateTime)
 
+data class Freezing(
+    override val stationId: String,
+    override val dateTime: LocalDateTime,
+    val hasFreezing: Boolean,
+) : WeatherStationStats(stationId, dateTime)
+
 class WeatherServer {
     fun sendMessage(message: WeatherStationStats) {
         when (message) {
             is Temperature -> sendTemperatureData(message)
             is PrecipitationAmount -> sendPrecipitationData(message)
+            else -> println("Ошибка отправки данных с ${message.stationId} Неизвестный тип данных")
         }
     }
 
@@ -30,7 +37,7 @@ class WeatherServer {
     }
 
     private fun sendPrecipitationData(precipitation: PrecipitationAmount) {
-        println("Отправка количества осадков на сервер: $precipitation")
+        println("Отправка количества осадков на сервер: ${precipitation.stationId}")
     }
 }
 
@@ -38,8 +45,10 @@ fun main() {
     val dateNow = LocalDateTime.now()
     val temperatureData = Temperature("Station1", dateNow, 25.5)
     val precipitationData = PrecipitationAmount("Station1", dateNow, 10.2)
+    val freezing = Freezing("station1", dateNow, false)
 
     val server1 = WeatherServer()
     server1.sendMessage(temperatureData)
     server1.sendMessage(precipitationData)
+    server1.sendMessage(freezing)
 }
