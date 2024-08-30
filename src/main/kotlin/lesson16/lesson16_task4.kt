2070a7f6ea890189fec5579c86042c9885ef199b
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-08-30 22:51:29 +0300

    KS-16-04-internet-shop-order 1) Создан класс для заказа. 2) Номер заказа генерится случайным образом и недоступен извне 3) Публичный метод requestToChangeStage запроса на изменения заказа. Возвращает изменена стадия или нет 4) приватный метод acceptRequestByManager подтверждения. Пока "заглушка" 5) Стадии заказ отдельным enum Stage

--- src/main/kotlin/lesson16/lesson16_task4.kt
+++ src/main/kotlin/lesson16/lesson16_task4.kt
@@ -1,1 +1,36 @@
 package lesson16
+
+enum class Stage {
+    CONFIRMED,
+    PROCESSING,
+    DELIVERED,
+    CANCELED,
+    RETURNED,
+}
+
+class Order {
+    private val orderNum = (1..10_000).random()
+    private var readyStage = Stage.CONFIRMED
+
+    fun requestToChangeStage(stage: Stage): Boolean = acceptRequestByManager(stage)
+
+    fun getStage() {
+        println(readyStage)
+    }
+
+    /**
+     * Оценка возможности смены стадии менеджером и смена стадии, если возможно
+     * @return - true - установлена новая стадия, false - отказ в изменении
+     */
+    private fun acceptRequestByManager(stage: Stage): Boolean {
+        readyStage = stage
+        return true
+    }
+}
+
+fun main() {
+    val order = Order()
+    order.getStage()
+    order.requestToChangeStage(Stage.PROCESSING)
+    order.getStage()
+}
