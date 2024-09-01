dbdf91e6d66d34f043c7e8f6e61f575b39e59cf8
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-01 16:24:16 +0300

    KS-16-05-RPG 1) Создан базовый класс для персонажа. В конструкторе можно установить базовые характеристики здоровья и силы удара и количества здоровья которое восстанавливается по умолчанию 2) Интерфейс BaseCharacterActions реализует базовые действия для всех персонажей. 3) Класс Warrior наследник Character и реализующий BaseCharacterActions

--- src/main/kotlin/lesson16/lesson16_task5.kt
+++ src/main/kotlin/lesson16/lesson16_task5.kt
@@ -1,1 +1,72 @@
 package lesson16
+
+abstract class Character protected constructor(
+    protected var name: String,
+    protected var health: Int,
+    protected var damage: Int,
+    protected var healVal: Int,
+) {
+    protected abstract fun isDead(param: Int): Boolean
+}
+
+interface BaseCharacterActions {
+    fun heal()
+
+    fun getDamage(getDamage: Int): Boolean
+}
+
+class Warrior(
+    name: String,
+    health: Int,
+    damage: Int,
+    healVal: Int,
+) : Character(name, health, damage, healVal),
+    BaseCharacterActions {
+    private var canHeal: Boolean = true
+
+    override fun heal() {
+        if (canHeal) {
+            println("$name  восстанавливает $healVal ед. здоровья")
+            health += healVal
+        }
+    }
+
+    /**
+     * Количество полученного дамага от другого персонажа
+     */
+    override fun getDamage(getDamage: Int): Boolean {
+        println("$name  получает  $getDamage ед. урона")
+        health -= getDamage
+        return isDead(health)
+    }
+
+    override fun isDead(param: Int): Boolean =
+
+        when {
+            health <= 0 -> {
+                canHeal = false
+                damage = 0
+                health = 0
+                println("$name ++RIP++")
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
+}
+
+fun main() {
+    val warrior = Warrior("TaNGaR", 100, 60, 20)
+
+    while (true) {
+        if (warrior.getDamage(40)) break
+
+        warrior.heal()
+
+        if (warrior.getDamage(70)) break
+
+        if (warrior.getDamage(30)) break
+    }
+}
