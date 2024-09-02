3272cc2133b39ba79a06dc4f2ab2a22a5fbd6293
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-02 23:26:04 +0300

    KS-17-02-ship 1) Класс Ship. В конструкторе передаются параметры для присвоения соответствующим полям 2) Для поля name переопределен сеттер, так что при попытке задать новое имя выводится сообщение о том, что это сделать невозможно

--- src/main/kotlin/lesson17/lesson17_task2.kt
+++ src/main/kotlin/lesson17/lesson17_task2.kt
@@ -1,1 +1,31 @@
 package lesson17
+
+class Ship(
+    name: String,
+    averageSpeed: Int,
+    homePort: String,
+) {
+    var name = name
+        get() = field
+        set(value) {
+            println("Имя корабля изменить нельзя")
+        }
+    var averageSpeed: Int = averageSpeed
+        get() = field
+        set(value) {
+            field = value
+        }
+    var homePort = homePort
+        get() = field
+        set(value) {
+            field = value
+        }
+}
+
+fun main() {
+    val ship = Ship("Крузенштерн", 15, "Простоквашино")
+    println(ship.name)
+    println(ship.averageSpeed)
+    println(ship.homePort)
+    ship.name = "Печкин"
+}
