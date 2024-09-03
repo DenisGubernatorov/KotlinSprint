859f6e5f8059cd2a445ae7289b43bc783deaae0d
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-03 22:51:09 +0300

    KS-17-04-geo-location 1) Класс посылки. Номер посылки присваивается при инициализации экземпляра класса на основе hashCode 2) При каждом изменении pacLocation увеличивается счетчик перемещений

--- src/main/kotlin/lesson17/lesson17_task4.kt
+++ src/main/kotlin/lesson17/lesson17_task4.kt
@@ -1,1 +1,23 @@
 package lesson17
+
+class Package(
+    pacLocation: String,
+) {
+    val pacNumber = this.hashCode()
+    var pacLocation = pacLocation
+        set(value) {
+            field = value
+            movementCounter++
+        }
+    var movementCounter = 0
+}
+
+fun main() {
+    val package1 = Package("Moscow")
+    println(package1.pacLocation)
+    println(package1.movementCounter)
+
+    package1.pacLocation = "Saint-Petersburg"
+    println(package1.pacLocation)
+    println(package1.movementCounter)
+}
