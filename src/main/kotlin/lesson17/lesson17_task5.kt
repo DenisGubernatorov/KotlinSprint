74f59dc9178df45226b8930967eb8ed3d5e24440
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-03 23:31:10 +0300

    KS-17-05-login-and-password 1) Класс User17. В конструкторе передаются логин и пароль 2) При попытке вывести пароль возвращается строка из звездочек. Количество звездочек соответствует длине пароля 3) При попытке установить пароль выводится сообщение, чо пароль изменить нельзя 4) При изменении логина дополнительно выводится сообщение о том, что логин успешно изменен.

--- src/main/kotlin/lesson17/lesson17_task5.kt
+++ src/main/kotlin/lesson17/lesson17_task5.kt
@@ -1,1 +1,30 @@
 package lesson17
+
+class User17(
+    userLogin: String,
+    userPassword: String,
+) {
+    var userLogin = userLogin
+        set(value) {
+            val oldVal = field
+            field = value
+            println("Логин \"$oldVal\" успешно изменен на \"$field\"")
+        }
+    var userPassword = userPassword
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+        get() {
+
+            return "*".repeat(field.length)
+        }
+}
+
+fun main() {
+    val user17 = User17("JustNiceLogin", "nicePassword")
+    println(user17.userLogin)
+    println(user17.userPassword)
+
+    user17.userLogin = "Login"
+    user17.userPassword = "NewPass"
+}
