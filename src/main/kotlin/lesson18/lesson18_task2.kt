95f62c0ba19c7c2a4623268ac86d9104b89e32e0
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-04 23:06:37 +0300

    KS-18-02-playing-dice 1) Базовый класс PlayingDiceL18 с базовым количеством граней и пустой реализацией метода отвечающего за бросок 2) Классы наследники с 4,6 и 8 гранями. В зависимости от класса метод getThrowResult берет рандомное число в своем диапазоне

--- src/main/kotlin/lesson18/lesson18_task2.kt
+++ src/main/kotlin/lesson18/lesson18_task2.kt
@@ -1,1 +1,36 @@
 package lesson18
+
+open class PlayingDiceL18 {
+    open val maxEdgeCount = 0
+
+    open fun getThrowResult() {}
+}
+
+class FourEdgeDice : PlayingDiceL18() {
+    override val maxEdgeCount = 4
+
+    override fun getThrowResult() {
+        println((1..maxEdgeCount).random())
+    }
+}
+
+class SixEdgeDice : PlayingDiceL18() {
+    override val maxEdgeCount = 6
+
+    override fun getThrowResult() {
+        println((1..maxEdgeCount).random())
+    }
+}
+
+class EightEdgeDice : PlayingDiceL18() {
+    override val maxEdgeCount = 8
+
+    override fun getThrowResult() {
+        println((1..maxEdgeCount).random())
+    }
+}
+
+fun main() {
+    val dices = mutableListOf(FourEdgeDice(), SixEdgeDice(), EightEdgeDice())
+    dices.forEach { it.getThrowResult() }
+}
