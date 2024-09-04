c11269acc06d0534726dbb93872479b986fc2c1d
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-04 23:36:01 +0300

    KS-18-03-tamagochi 1) Общий класс TamagochiAnimal с пустой реализацией метода eat 2) Классы Fox, Dog, Cat наследники общего класса и переопределяющие метод eat

--- src/main/kotlin/lesson18/lesson18_task3.kt
+++ src/main/kotlin/lesson18/lesson18_task3.kt
@@ -1,1 +1,36 @@
 package lesson18
+
+open class TamagochiAnimal(
+    open val name: String,
+) {
+    open fun eat() {}
+}
+
+class Fox(
+    override val name: String,
+) : TamagochiAnimal(name) {
+    override fun eat() {
+        println("$name -> ест ягоды")
+    }
+}
+
+class Dog(
+    override val name: String,
+) : TamagochiAnimal(name) {
+    override fun eat() {
+        println("$name -> ест кости")
+    }
+}
+
+class Cat(
+    override val name: String,
+) : TamagochiAnimal(name) {
+    override fun eat() {
+        println("$name -> ест рыбу")
+    }
+}
+
+fun main() {
+    val animals: MutableList<TamagochiAnimal> = mutableListOf(Fox("Foxy"), Dog("Jack"), Cat("Beast"))
+    animals.forEach { it.eat() }
+}
