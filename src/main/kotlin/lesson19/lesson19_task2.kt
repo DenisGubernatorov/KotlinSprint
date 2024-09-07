4210e4b93cf8c40f6cd8cac355bcd2f4091140cf
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-07 21:06:43 +0300

    KS-19-02-categories 1) Enum Category с категориями товаров и методом возвращающим описание категории 2) Класс Item19 для товара. Со свойствами для названия, ид и категории. Метод для вывода информации о товаре

--- src/main/kotlin/lesson19/lesson19_task2.kt
+++ src/main/kotlin/lesson19/lesson19_task2.kt
@@ -1,1 +1,34 @@
 package lesson19
+
+enum class Category {
+    CLOTHES,
+    STATIONERY,
+    OTHER,
+    ;
+
+    fun description(): String =
+        when (this) {
+            CLOTHES -> "Одежда"
+            STATIONERY -> "Канцелярские товары"
+            OTHER -> "Остальное"
+        }
+}
+
+class Item19(
+    private val name: String,
+    private var category: Category,
+) {
+    private val id = this.hashCode()
+
+    fun itemDescription() {
+        println("Название: $name\nИД:$id\nКатегория: ${category.description()}")
+    }
+}
+
+fun main() {
+    Item19("Item1", Category.CLOTHES).itemDescription()
+    println()
+    Item19("Item2", Category.STATIONERY).itemDescription()
+    println()
+    Item19("Item3", Category.OTHER).itemDescription()
+}
