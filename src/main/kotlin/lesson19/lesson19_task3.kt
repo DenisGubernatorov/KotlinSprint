cb95bb565e8c266bae05e1ad1a9426885af0c86a
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-07 21:34:32 +0300

    KS-19-03-todo 1) Прототип класса Starship с нереализованными методами и использованием разных вариантов todo

--- src/main/kotlin/lesson19/lesson19_task3.kt
+++ src/main/kotlin/lesson19/lesson19_task3.kt
@@ -1,1 +1,24 @@
 package lesson19
+
+class Starship(
+    private val starshipName: String,
+) {
+    fun launch() {
+        // TODO Необходима информация для реализации необходимой логики
+    }
+
+    fun landing() {
+        println("$starshipName  приземляется")
+    }
+
+    fun meteorDefence() {
+        TODO("Реализовать логику защиты от метеоритов")
+    }
+}
+
+fun main() {
+    val starship = Starship("Авангард")
+    starship.launch()
+    starship.landing()
+    starship.meteorDefence()
+}
