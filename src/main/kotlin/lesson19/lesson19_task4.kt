6ac487e4ce02ea1983f6438a03e930abe0c54677
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-08 21:41:08 +0300

    KS-19-04-tank 1) Энум с типами зарядов и их уроном 2) Класс танк. При создании экземпляра танка он ничем не заряжен 3) В методе main проходится по всем типов снарядов из энума, заряжает соответствующий заряд и производит выстрел

--- src/main/kotlin/lesson19/lesson19_task4.kt
+++ src/main/kotlin/lesson19/lesson19_task4.kt
@@ -1,1 +1,32 @@
 package lesson19
+
+enum class Ammunition(
+    private val damage: Int,
+) {
+    BLUE(5),
+    GREEN(10),
+    RED(20),
+    ;
+
+    fun getDamage(ammunition: Ammunition): Int = ammunition.damage
+}
+
+class Tank {
+    private var damage = 0
+
+    fun loadAmmo(ammunition: Ammunition) {
+        damage = ammunition.getDamage(ammunition)
+    }
+
+    fun shoot() {
+        println("Танк стреляет снарядом с уроном $damage")
+    }
+}
+
+fun main() {
+    val tank = Tank()
+    Ammunition.entries.forEach {
+        tank.loadAmmo(it)
+        tank.shoot()
+    }
+}
