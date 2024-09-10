42c6e02db4a7a26edc291e4de746079a4f5df7d8
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-10 23:48:09 +0300

    KS-20-05-inverter 1) Класс InverterRobot с методами say(), setModifier(), списком фраз и не инициализированным полем inverter, принимающим лямбду 2) Метод say() печатает фразу из лямбды если inverter не null или случайную фразу из списка фраз 3) setModifier устанавливает лямбду, которая берет случайную фразу из списка фраз и возвращает ее в перевернутом виде

--- src/main/kotlin/lesson20/lesson20_task5.kt
+++ src/main/kotlin/lesson20/lesson20_task5.kt
@@ -1,1 +1,30 @@
 package lesson20
+
+class InverterRobot {
+    private var inverter: (() -> String)? = null
+    private val phrases =
+        listOf(
+            "Жизнь! Не говорите мне о жизни...",
+            "Я в глубокой депрессии.",
+            "Как только жизнь становится лучше, она снова ухудшается.",
+            "Вы не представляете, что значит быть роботом с депрессией.",
+            "Лежать лицом в пыли — отличный способ отчаиваться.",
+        )
+
+    fun say() {
+        println(inverter?.invoke() ?: phrases.random())
+    }
+
+    fun setModifier() {
+        inverter = {
+            phrases.random().reversed()
+        }
+    }
+}
+
+fun main() {
+    val inverterRobot = InverterRobot()
+    inverterRobot.say()
+    inverterRobot.setModifier()
+    inverterRobot.say()
+}
