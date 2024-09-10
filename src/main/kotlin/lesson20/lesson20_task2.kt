b3c99aea414ceb9082062cb39b0511aa9595fdc9
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-10 21:38:17 +0300

    KS-20-02-healing 1) Класс Player20  со свойствами имя, текущее здоровье и максимальное значение 2) Лямбда с явным указанием типа и принимающая на вход экземпляр Player20. Функция восполняет текущее здоровье до максимального 3) В main вывод текущего здоровья до применения лямбды и после

--- src/main/kotlin/lesson20/lesson20_task2.kt
+++ src/main/kotlin/lesson20/lesson20_task2.kt
@@ -1,1 +1,16 @@
 package lesson20
+
+class Player20(
+    var name: String,
+    var currentHealth: Int,
+) {
+    val maxHealth: Int = 100
+}
+
+fun main() {
+    val healFun: (player: Player20) -> Unit = { player -> player.currentHealth = player.maxHealth }
+    val player20 = Player20("Tan", 30)
+    println(player20.currentHealth)
+    healFun(player20)
+    println(player20.currentHealth)
+}
