79d1c15c19bd13dd5b2276d6bfa2e5355be1523b
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-10 22:46:49 +0300

    KS-20-04-Elements 1) Преобразование списка строк в список лямбд и вызов четных лямбд из преобразованного списка

--- src/main/kotlin/lesson20/lesson20_task4.kt
+++ src/main/kotlin/lesson20/lesson20_task4.kt
@@ -1,1 +1,14 @@
 package lesson20
+
+fun main() {
+    val elements = mutableListOf("Элемент0", "Элемент1", "Элемент2", "Элемент3", "Элемент4")
+
+    elements
+        .map {
+            { println("Нажат элемент $it") }
+        }.forEachIndexed { index, it ->
+            if (index % 2 == 0) {
+                it()
+            }
+        }
+}
