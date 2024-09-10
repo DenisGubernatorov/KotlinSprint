107fe545567d2f723b99c1989884ff03192dc283
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-10 21:54:34 +0300

    KS-20-03-Door 1) Класс PlayerWithKey с единственным полем, есть ли ключ 2) Лямбда принимающая на вход объект PlayerWithKey и, в зависимости от наличия ключа, выводящая сообщение открыта дверь или нет

--- src/main/kotlin/lesson20/lesson20_task3.kt
+++ src/main/kotlin/lesson20/lesson20_task3.kt
@@ -1,1 +1,13 @@
 package lesson20
+
+class PlayerWithKey(
+    var haveKey: Boolean,
+)
+
+fun main() {
+    val isHaveKey: (player: PlayerWithKey) -> Unit =
+        { player -> println(if (player.haveKey) "Дверь открыта" else "Все еще закрыто") }
+    isHaveKey(PlayerWithKey(true))
+
+    isHaveKey(PlayerWithKey(false))
+}
