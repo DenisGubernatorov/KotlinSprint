fc2acc3957c58cdb3dd1491fbc926e07360026a7
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-12 23:14:43 +0300

    KS-21-05-skills 1) Функция расширение для map, где ключ строка, а значение целое число. Возвращает ключ у которого максимальное значение. Если таких несколько, то вернет первый. Если мапа пустая, то вернет null

--- src/main/kotlin/lesson21/lesson21_task5.kt
+++ src/main/kotlin/lesson21/lesson21_task5.kt
@@ -1,1 +1,13 @@
 package lesson21
+
+fun main() {
+    val skills =
+        mutableMapOf(
+            "Skill_1" to 40,
+            "Skill_2" to 90,
+            "Skill_3" to 70,
+        )
+
+    fun MutableMap<String, Int>.maxCategory(): String? = maxByOrNull { it.value }?.key
+    println(skills.maxCategory())
+}
