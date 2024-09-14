c015f2c8b889f1c46e367359055e1085ee47afa6
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-14 13:30:30 +0300

    KS-22-05-components 1)Data class GalacticGuide со свойствами согласно ТЗ 2) Выводе значения свойств с использованием компонентных функций

--- src/main/kotlin/lesson22/lesson22_task5.kt
+++ src/main/kotlin/lesson22/lesson22_task5.kt
@@ -1,1 +1,21 @@
 package lesson22
+
+import java.time.LocalDateTime
+
+data class GalacticGuide(
+    val eventName: String,
+    val description: String,
+    val dateTime: LocalDateTime,
+    val distance: Double,
+)
+
+fun main() {
+    val galacticGuide =
+        GalacticGuide(
+            "Земля",
+            "Уничтожение для строительства межгалактической магистрали",
+            LocalDateTime.now(),
+            0.0,
+        )
+    println("${galacticGuide.component1()}\n${galacticGuide.component2()}\n${galacticGuide.component3()}\n${galacticGuide.component4()}\n ")
+}
