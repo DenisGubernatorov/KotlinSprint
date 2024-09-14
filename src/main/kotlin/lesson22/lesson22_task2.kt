9c2d9ab149b993abba7f57bdbe1ecc20640e0a14
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-14 12:43:27 +0300

    KS-22-02-toString 1) Сравнение toString  для обычных и data классов

--- src/main/kotlin/lesson22/lesson22_task2.kt
+++ src/main/kotlin/lesson22/lesson22_task2.kt
@@ -1,1 +1,21 @@
 package lesson22
+
+class RegularBook1(
+    val title: String,
+    val author: String,
+)
+
+data class DataBook1(
+    val title: String,
+    val author: String,
+)
+
+fun main() {
+    println(RegularBook1("Title", "Author"))
+    println(DataBook1("Title", "Author"))
+}
+
+/*
+Разница в том, что стандартная реализация toString для обычных классов выводит на экран имя класса и хеш-код.
+А для data классов toString автоматические переопределяется и выводит значения всех заданных в конструкторе
+ */
