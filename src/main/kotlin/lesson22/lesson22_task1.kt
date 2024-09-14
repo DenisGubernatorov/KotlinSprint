2f7951bdafe6a297bfbf45896daeddc5a8dacdfd
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-14 11:53:58 +0300

    KS-22-01-regular-Class-vs-data-class 1) Пример сравнения объектов обычного класса и data класса 2) Комментарий по поводу нюансов работы сравнения объектов

--- src/main/kotlin/lesson22/lesson22_task1.kt
+++ src/main/kotlin/lesson22/lesson22_task1.kt
@@ -1,1 +1,30 @@
 package lesson22
+
+class RegularBook(
+    val title: String,
+    val author: String,
+)
+
+data class DataBook(
+    val title: String,
+    val author: String,
+)
+
+fun main() {
+    val regularBook = RegularBook("regBook1", "Author1")
+    val regularBook1 = RegularBook("regBook1", "Author1")
+
+    val dataBook = DataBook("dataBook1", "dAuthor1")
+    val dataBook1 = DataBook("dataBook1", "dAuthor1")
+
+    println(regularBook == regularBook1)
+    println(dataBook == dataBook1)
+    /*
+    Оператор == "под капотом" использует equals. В случае для обычных классов, наследников Object, сравниваются ссылки на объекты, если мы сами его
+    не переопределяем.
+    Для data классов автоматически переопределяется eqauls, на основе свойств переданных в конструкторе. И сравнивает их значение. Таким образом,
+    если у двух объектов data класса одинаковые значения свойств, то они считаются одинаковыми. Это накладывает ограничения на первичный конструктор.
+    В нем должно быть определено, как минимум, одно свойство. Также, свойства, объявленные в теле data класса, а не в конструкторе, не участвуют
+    в переопределении equals
+     */
+}
