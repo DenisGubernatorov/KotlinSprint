22cb2d79a4230297c6d9fc4b7c35d9b816de1dba
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-14 13:03:44 +0300

    KS-22-03-destructurization 1) Примем деструктуризации свойств data класса

--- src/main/kotlin/lesson22/lesson22_task3.kt
+++ src/main/kotlin/lesson22/lesson22_task3.kt
@@ -1,1 +1,13 @@
 package lesson22
+
+data class DataExample(
+    val field1: String,
+    val field2: Int,
+    val field3: Boolean,
+)
+
+fun main() {
+    val dataExample = DataExample("str", 1, true)
+    val (fieldStr, fieldInt, fieldBoolean) = dataExample
+    println("$fieldStr $fieldInt $fieldBoolean")
+}
